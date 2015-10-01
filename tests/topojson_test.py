93e58f5e17218e26992bc09552e4ca8c56ba111f
Shaun Walbridge <shaun.walbridge@gmail.com>
2015-10-01 15:13:03 -0400

    use data files
     - push out the GeoJSON into its own file, easier to reuse and test
     - remove commented self-test

--- tests/topojson_test.py
+++ tests/topojson_test.py
@@ -3,35 +3,13 @@ import unittest
 
 from topojson.conversion import convert
 
+
 class TestTopojson(unittest.TestCase):
 
-    GeoJSON = json.loads("""
-        {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "properties": {},
-                    "geometry": {
-                        "type": "Polygon",
-                        "coordinates": [[
-                            [-10, 10],
-                            [ 10, 10],
-                            [ 10, -10],
-                            [-10, -10],
-                            [-10, 10]
-                        ]]
-                    }
-                }
-            ]
-        }
-    """)
-
     def setUp(self):
-        pass
+        with open("tests/data/square.geojson") as f:
+            self.square_geojson = json.load(f)
 
-    def test_topojson(self):
-        tj = convert(self.GeoJSON)
+    def test_convert_geojson_to_topojson(self):
+        tj = convert(self.square_geojson)
         self.assertEqual(tj['type'], 'Topology')
-        # with open('/tmp/out.topojson', 'w') as f:
-            # json.dump(tj, f)
