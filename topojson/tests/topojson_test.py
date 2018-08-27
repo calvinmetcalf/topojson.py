bb57f0e630ba22d76e112d6903f05a38c7367434
Philipp Kats <casyfill@gmail.com>
2018-08-26 22:56:58 -0400

    adding tests

--- topojson/tests/topojson_test.py
+++ topojson/tests/topojson_test.py
@@ -3,31 +3,39 @@ import pytest
 from os import path
 import requests as rq
 from .utils import validate_json
+from glob import glob
 
-test_dir = path.join(path.dirname(__file__), 'data')
+test_dir = path.join(path.dirname(__file__), "data")
 
 
-@pytest.fixture(scope='class')
+@pytest.fixture(scope="class")
 def tpscheme(request):
-    url = 'https://raw.githubusercontent.com/calvinmetcalf/topojson.py/v1.1.x/test_objects/schema/topojson.json'
+    url = "https://raw.githubusercontent.com/calvinmetcalf/topojson.py/v1.1.x/test_objects/schema/topojson.json"
     request.cls.tpscheme = rq.get(url).json()
 
 
-@pytest.fixture(scope='function')
-def square():
-    with open(path.join(test_dir, 'square.geojson')) as f:
-            square = json.load(f)
-
-    return square
+objs = {
+    "square": {"type": "Topology", "bbox_length": 4, "geometries": 1},
+    "square_with_hole": {"type": "Topology", "bbox_length": 4, "geometries": 1},
+    "two_squares": {"type": "Topology", "bbox_length": 4, "geometries": 2},
+}
 
 
 @pytest.mark.usefixtures("tpscheme")
 class TestTopojson(object):
 
-    def test_convert_geojson_to_topojson(self, square):
+    @pytest.mark.parametrize("type, props", objs.items())
+    def test_convert_geojson_to_topojson(self, type, props):
         from topojson.conversion import convert
 
-        tj = convert(square)
-        print(tj)
-        assert tj['type'] == 'Topology'
-        assert validate_json(tj, self.tpscheme)
+        with open(path.join(test_dir, f'{type}.geojson'), 'r') as f:
+            obj = json.load(f)
+
+        tj = convert(obj)
+
+        # check vs schema
+        validate_json(tj, self.tpscheme)
+
+        assert tj["type"] == props['type']
+        assert len(tj["bbox"]) == props['bbox_length']
+        assert len(tj["objects"]["name"]["geometries"]) == props['geometries']
